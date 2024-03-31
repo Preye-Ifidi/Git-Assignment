eaa2ea50a51a5daaaf668c7479bb3f31ecdfda3a
Preye-Ifidi <ifidivictoria@yahoo.com>
2024-03-31 23:34:33 +0100

    changed color of footer and button

--- src/index.js
+++ src/index.js
@@ -108,4 +108,4 @@ function displayForecast(response) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Nigeria");
+searchCity("Paris");
